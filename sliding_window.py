a00a5f13b64712c1cfacc4f70b9944217ba37b8f
Catherine Yee <yeeca@seas.upenn.edu>
2016-11-06 16:34:51 -0500

    started sliding stuff

--- sliding_window.py
+++ sliding_window.py
@@ -7,13 +7,37 @@
 import numpy as np
 from itertools import islice
 
-def window(seq, n=100):
-    "Returns a sliding window (of width n) over data from the iterable"
-    "   s -> (s0,s1,...s[n-1]), (s1,s2,...,sn), ...                   "
-    it = iter(seq)
-    result = tuple(islice(it, n))
-    if len(result) == n:
-        yield result    
-    for elem in it:
-        result = result[1:] + (elem,)
-        yield result
+class slidingWindow:
+
+    def __init__(self, width=100, shift=50):
+   	    '''
+        Constructor
+        '''
+        self.width = width
+        self.shift = shift
+
+	def window_idx(self, X):
+    	"Returns indices of a sliding window over data from the iterable"
+    	"   s -> (s0,s1,...s[width-1]), (s1,s2,...,s_width), ..."
+    	"(n/shift) x width"
+    	
+    	n,d = X.shape
+
+    	seq = xrange(n)
+    	
+    	it = iter(seq)
+    	result = tuple(islice(it, self.width))
+    	
+    	if len(result) == self.width:
+        	yield result    
+    	for i in range(0: len(seq)/self.shift)
+    		result = result[self.shift:] + tuple(islice(iter(seq(i*self.shift:))), self.width)
+        	yield result
+
+    def window_dat(self, X):
+    	"Returns (n/shift) x (width x d) with Xtrain data"
+
+    	n,d = X.shape
+
+    	indices = self.window_idx(X)
+    	data_windows = np.zeros(len(indices), self.width, d)
--- test_sliding_window.py
+++ test_sliding_window.py
@@ -1,18 +1,26 @@
-'''
+"""
 	CIS 519 Project: Seizure Prediction
 	Sliding Window Algorithm TEST
 	Author: Catherine Yee
-'''
-
+"""
+print(__doc__)
 import numpy as np
-from sliding_window import window
+from sliding_window import slidingWindow
 
-filePath = ".dat"
+#width = 100;
+
+filePath = "1_1_0_small.dat"
 file = open(filePath,'r')
 allData = np.loadtxt(file, delimiter=',')
 
 Xtrain = allData
+# Ytrain = 
 
+n,d = Xtrain.size
 print "Xtrain.size = ", Xtrain.size
 
-windows = 
+test_sw = slidingWindow(width=100, shift=50)
+test_indices = test_sw.window_idx(Xtrain)
+
+for i in range(0, 5):
+	print "indices = ", test_indices[i,:]
