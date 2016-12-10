9a68c15cd0d66f782a6b4363d1c9f3f2d7fe3532
Tyrell McCurbin <tmccurbin@hotmail.com>
2016-12-10 15:43:35 -0500

    updated nonlinear energy

--- energy_calcs.py
+++ energy_calcs.py
@@ -33,15 +33,17 @@ def nonlinear_energy(X):
     '''
     
     num_data_pts = int(X.shape[0]);
-    number_of_channels = int(X.shape[1]);
-    nonlinear_energy = np.zeros([1, number_of_channels]); # initialize final array
+#    number_of_channels = int(X.shape[1]);
+#    nonlinear_energy = np.zeros([1, number_of_channels]); # initialize final array
+#    
+#    # Output a vector of eneries. Each value corresponds to the channel's energy
+#    for channel in range (number_of_channels):
+#        for i in range(1, num_data_pts - 1): # START WITH THE SECOND DATA POINT
+#            nonlinear_energy[0, channel] += (X[i][channel])**2 - (X[i - 1][channel] * X[i + 1][channel]);
+#    
+#    return nonlinear_energy / num_data_pts;
     
-    # Output a vector of eneries. Each value corresponds to the channel's energy
-    for channel in range (number_of_channels):
-        for i in range(1, num_data_pts - 1): # START WITH THE SECOND DATA POINT
-            nonlinear_energy[0, channel] += (X[i][channel])**2 - (X[i - 1][channel] * X[i + 1][channel]);
-    
-    return nonlinear_energy / num_data_pts; 
+    return np.sum(X[1:-1,:]**2 - np.multiply(X[0:-2,:], X[2:,:]), axis=0) / num_data_pts;
 
 def accumulated_energy(X):
     '''
