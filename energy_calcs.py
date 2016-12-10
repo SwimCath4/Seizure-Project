6e6a57276c24f738f47ff34002566304705ad6fc
Catherine Yee <yeeca@seas.upenn.edu>
2016-12-10 14:51:45 -0500

    vectorized energy calculation

--- energy_calcs.py
+++ energy_calcs.py
@@ -12,17 +12,19 @@ def energy(X):
     Author: Catherine
     '''
     
-    num_data_pts = int(X.shape[0]);
-    number_of_channels = int(X.shape[1]);
-    energy = np.zeros([1, number_of_channels]); # initialize final array
+    #num_data_pts = int(X.shape[0]);
+    #number_of_channels = int(X.shape[1]);
+    #energy = np.zeros([1, number_of_channels]); # initialize final array
     
     # Output a vector of eneries. Each value corresponds to the channel's energy
-    for channel in range (number_of_channels):
-        for i in range(1, num_data_pts - 1): # START WITH THE SECOND DATA POINT
-            energy[0, channel] += (X[i][channel])**2;
-        energy[0, channel] = (energy[0, channel]) / num_data_pts;
+    #for channel in range (number_of_channels):
+    #    for i in range(1, num_data_pts - 1): # START WITH THE SECOND DATA POINT
+    #        energy[0, channel] += (X[i][channel])**2;
+    #    energy[0, channel] = (energy[0, channel]) / num_data_pts;
     
-    return energy
+    #return energy
+
+    return np.mean(X**2, axis = 0).reshape(1,16);
 
 def nonlinear_energy(X):
     '''
@@ -55,6 +57,5 @@ def accumulated_energy(X):
     for channel in range (number_of_channels):
         for i in range(1, num_data_pts - 1): # START WITH THE SECOND DATA POINT
             accumulated_energy[0, channel] += (X[i][channel])**2;
-            #I still need to finish coding this computation
     
     return accumulated_energy
