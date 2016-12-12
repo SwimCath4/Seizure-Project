e70b3f570b805bc454fe854e79c5a3c17d3c6db2
Zheng Tian <zheng718@gmail.com>
2016-12-12 05:22:39 -0500

    assign variable to be the Xtrain returned by the fit_transform()

--- main.py
+++ main.py
@@ -29,14 +29,14 @@ n_components = 20
 
 # dimensionality reduction; change the method for whichever method
 dim_red_method = dim_red.lle(n_components)
-dim_red_method.fit_transform(Xtrain[:,0:-1], ytrain)
+reducedXTrain = dim_red_method.fit_transform(Xtrain[:,0:-1], ytrain)
 
 Xtest = allData[int(len(allData)/2) + 1:, :]
 Xtest[:,0:-1] = standardize(Xtest[:,0:-1])
 dim_red_method.transform(Xtest[:,0:-1])
 ytest = allData[int(len(allData)/2) + 1:, -1]
 
-n_train, d = Xtrain.shape
+n_train, d = reducedXTrain.shape
 n_test = Xtest.shape[0]
 
 centroid_0 = np.zeros((1,n_train-1))
@@ -52,26 +52,28 @@ centroid_1_init = 0
 for i in range(0, n_train):
     if(ytrain[i] == 0.0):
         if(centroid_0_init == 0):
-            centroid_0 = Xtrain[i,:-1]
+            centroid_0 = reducedXTrain
             centroid_0_init = 1
             #print "centroid_0 first = ", centroid_0
         else:
-            centroid_0 = compute_centroid(centroid_0, Xtrain[i,:-1])
+            # centroid_0 = compute_centroid(centroid_0, Xtrain[i,:-1])
+            centroid_0 = compute_centroid(centroid_0, reducedXTrain)
 
     elif(ytrain[i] == 1.0):
         if(centroid_1_init == 0):
-            centroid_1 = Xtrain[i,:-1]
+            centroid_1 = reducedXTrain
             centroid_1_init = 1
             #print "centroid_1 first = ", centroid_1
         else:
-            centroid_1 = compute_centroid(centroid_1, Xtrain[i,:-1])
+            # centroid_1 = compute_centroid(centroid_1, Xtrain[i,:-1])
+            centroid_1 = compute_centroid(centroid_1, reducedXTrain)
 
 #print "centroid_0 = ", centroid_0
 #print "centroid_1 = ", centroid_1
 
 ## Predict testing data ##
 for i in range(0, n_test):
-    predicted_labels[i] = predict(centroid_0, centroid_1, Xtest[i,:-1])
+    predicted_labels[i] = predict(centroid_0, centroid_1, Xtest[i,:])
 
 print "predicted_labels = ", predicted_labels
 
